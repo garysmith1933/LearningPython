62ca27de7126c910bdc8e67fffd58eee851a2763
Gary Smith <garysmith193358@gmail.com>
2023-01-26 16:57:22 -0500

    added notes for strings

---
+++ variables_and_strings.py
@@ -0,0 +1,22 @@
+#Variables
+my_income = 100
+tax_rate = 0.1
+
+my_taxes = tax_rate * my_income
+
+print(tax_rate)
+
+#should be lowercase for readability
+#variables can be dynamically typed
+#can find type of variable using type()
+
+
+# Strings
+
+# use \n in prints to make a new line
+# \t in prints make a tab
+print("Hi \nGary")
+print ("Good \t Day")
+
+# len checks the length of a string. 
+print(len("I like food"))
