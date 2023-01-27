ddf927f7228ab7348078abd84957a1d35ca07f58
Gary Smith <garysmith193358@gmail.com>
2023-01-27 08:47:15 -0500

    organized files into folders, added notes and examples for if else statements

---
+++ statements/if_else.py
@@ -0,0 +1,25 @@
+gary = 'gar'
+
+if gary == 'gary': 
+  print('YES BOY')
+
+else: 
+  print('Nah')
+
+  loc = 'Bank'
+
+  if loc == 'Auto Shop': 
+    print('I do like cars')
+
+  elif loc == 'Bank':
+    print('Help im broke')
+
+  elif loc == 'Store':
+    print('Go get groceries')
+
+  else: 
+    print('im screwed')
+
+# elif is the same as else if in javascript.
+# curly brackets are replaced with colons
+# indentation is CRUCIAL for readability
