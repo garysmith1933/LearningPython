92ef45b9da3f13176e3925ea7960a67f6ff991b2
Gary Smith <garysmith193358@gmail.com>
2023-01-26 20:44:16 -0500

    added notes for strings, seperated from variables

---
+++ strings.py
@@ -0,0 +1,26 @@
+# Strings
+
+# use \n in prints to make a new line
+# \t in prints make a tab
+#print("Hi \nGary")
+#print ("Good \t Day")
+
+# len checks the length of a string. 
+#print(len("I like food"))
+
+#getting a specific index in a string is the same as javascript
+mystring = "hello world"
+#print(mystring[0])
+
+newstr = 'abcdef'
+#you can get every index including the second this way
+print(newstr[2:])
+
+#you can get indexes before but not including the 3rd this way
+print(newstr[:3])
+
+#will log from 1 to 4
+print(newstr[1:4])
+
+#will log the string in reverse
+print(newstr[::-1])
--- variables.py
+++ variables.py
@@ -9,14 +9,3 @@ print(tax_rate)
 #should be lowercase for readability
 #variables can be dynamically typed
 #can find type of variable using type()
-
-
-# Strings
-
-# use \n in prints to make a new line
-# \t in prints make a tab
-print("Hi \nGary")
-print ("Good \t Day")
-
-# len checks the length of a string. 
-print(len("I like food"))
