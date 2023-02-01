9a33330de218105acd95bc22ce15f019848f7f34
Gary Smith <garysmith193358@gmail.com>
2023-02-01 10:39:17 -0500

    added notes and examples on inheritance

---
+++ oop/interit.py
@@ -0,0 +1,29 @@
+class Animal():
+
+  def __init__(self):
+    print("ANIMAL CREATED")
+
+  def who_am_i(self):
+    print("I am an animal")
+
+  def eat(self):
+    print("I am eating")
+
+myanimal = Animal()
+
+#Derive Class
+class Dog(Animal):
+  
+  def __init__(self):
+    Animal.__init__(self)
+    print("Dog Created")
+  
+  def who_am_i(self):
+    print("Im a dog")
+
+mydog = Dog()
+mydog.eat()
+mydog.who_am_i() 
+
+# the dog class now has all methods from the animal class
+# these methods can be over written by initalizing a method with the same name on the derive class
