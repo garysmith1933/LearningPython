5eed429d55ef8b67eb3a284a7c0ed215bf2d9d28
unknown <garysmith193358@gmail.com>
2023-02-28 17:43:09 -0500

    made borders thicker, working on styling box based on right or wrong answer

--- mini_proj/client/src/App.js
+++ mini_proj/client/src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+const isCorrect = (choice, answer) => {
+  if (choice === answer) {
+    console.log("correct")
+  }
+
+  else {
+    console.log('WRONG')
+  }
+}
+
 function App() {
   const [questions, setQuestions] = useState([])
 
@@ -21,21 +31,18 @@ function App() {
     questions.map((question) => {
     const [idx, title, option1, option2, option3, answer] = question
     // return <li key={idx}> {title} </li>
-    return <div key={idx}>
+    return <div key={idx} id='question-container'>
               <h3 className='question-title'>{title}</h3>
               <div className='questions'>
-                <div className='options'>{option1}</div>
+                  <div className='options' onClick={() => isCorrect(option1, answer)}>{option1}</div>
+              
                 <div className='options'>{option2}</div>
                 <div className='options'>{option3}</div>
                 <div className='options'>{answer}</div>
               </div>
           </div>
-
-    
   })
   
-
-
   return (
     <div className="App">
      <h1>Think you know me?</h1>
