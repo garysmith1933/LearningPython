1da1f444ac1e31c76cfc0bfa78f682c2f6824cd4
unknown <garysmith193358@gmail.com>
2023-03-01 10:38:44 -0500

    added functionality to remove colored borders when moving to next question

--- mini_proj/client/src/App.js
+++ mini_proj/client/src/App.js
@@ -16,9 +16,6 @@ const isCorrect = (element, choice, answer) => {
 function App() {
   const [questions, setQuestions] = useState([])
   const [current, setCurrent] = useState(0)
-  const answer = 0
-  let id, title, option1, option2, option3, option4
-
 
   useEffect(() => {
     fetch("http://localhost:8080/data")
@@ -27,22 +24,36 @@ function App() {
       data => {
         console.log(data)
         setQuestions(data)
-        option1 = questions[current][2]
       }
     )
   },[])
+
+  const nextQuestion = () => {
+    setCurrent(current + 1)
+    const elements = document.getElementsByClassName('options')
+    console.log(elements)
+
+    Array.from(elements).forEach(el => {
+      el.classList.remove("correct")
+      el.classList.remove("incorrect")
+    })
+    // for (element of elements) {
+    //   element.classList.remove("correct")
+    //   element.classList.remove("incorrect")
+    // }
+  }
 
 
   const data = questions.length === 0 ?
       <p>Loading...</p> 
 :
     <div id='question-container'>
-      <h3 className='question-title'>{title}</h3>
+      <h3 className='question-title'>{questions[current][1]}</h3>
         <div className='questions'>
-          <div className='options' id="option1" onClick={() => isCorrect("option1", option1, answer)}>{questions[current][2]}</div>
-            <div className='options' id="option2" onClick={() => isCorrect("option2", option2, answer)}>{questions[current][3]}</div>
-            <div className='options' id="option3" onClick={() => isCorrect("option3", option3, answer)}>{questions[current][4]}</div>
-            <div className='options' id="option4" onClick={() => isCorrect("option4", option4, answer)}>{questions[current][5]}</div>
+          <div className='options' id="option1" onClick={() => isCorrect("option1", questions[current][2], questions[current][5])}>{questions[current][2]}</div>
+            <div className='options' id="option2" onClick={() => isCorrect("option2", questions[current][3], questions[current][5])}>{questions[current][3]}</div>
+            <div className='options' id="option3" onClick={() => isCorrect("option3", questions[current][4], questions[current][5])}>{questions[current][4]}</div>
+            <div className='options' id="option4" onClick={() => isCorrect("option4", questions[current][5], questions[current][5])}>{questions[current][5]}</div>
           </div>
         </div>
   
@@ -52,7 +63,7 @@ function App() {
      <h4>Try out this quiz and see how you do!</h4>
      <ul>
       {data}
-      <button onClick={() => setCurrent(current + 1)}> Next </button>
+      <button onClick={() => nextQuestion()}> Next </button>
      </ul>
     </div>
   );
