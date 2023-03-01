ecbb87d343fc7355396325443164f56ed4b30b86
unknown <garysmith193358@gmail.com>
2023-03-01 09:36:38 -0500

    adjusted questions to be displayed one at a time

--- mini_proj/client/src/App.js
+++ mini_proj/client/src/App.js
@@ -15,6 +15,10 @@ const isCorrect = (element, choice, answer) => {
 
 function App() {
   const [questions, setQuestions] = useState([])
+  const [current, setCurrent] = useState(0)
+  const answer = 0
+  let id, title, option1, option2, option3, option4
+
 
   useEffect(() => {
     fetch("http://localhost:8080/data")
@@ -23,27 +27,24 @@ function App() {
       data => {
         console.log(data)
         setQuestions(data)
+        option1 = questions[current][2]
       }
     )
   },[])
 
+
   const data = questions.length === 0 ?
       <p>Loading...</p> 
 :
-    questions.map((question) => {
-    const answer = question[question.length-1]
-    const [idx, title, option1, option2, option3, option4] = question
-    // return <li key={idx}> {title} </li>
-    return <div key={idx} id='question-container'>
-              <h3 className='question-title'>{title}</h3>
-              <div className='questions'>
-                <div className='options' id="option1" onClick={() => isCorrect("option1", option1, answer)}>{option1}</div>
-                <div className='options' id="option2" onClick={() => isCorrect("option2", option2, answer)}>{option2}</div>
-                <div className='options' id="option3" onClick={() => isCorrect("option3", option3, answer)}>{option3}</div>
-                <div className='options' id="option4" onClick={() => isCorrect("option4", option4, answer)}>{option4}</div>
-              </div>
+    <div id='question-container'>
+      <h3 className='question-title'>{title}</h3>
+        <div className='questions'>
+          <div className='options' id="option1" onClick={() => isCorrect("option1", option1, answer)}>{questions[current][2]}</div>
+            <div className='options' id="option2" onClick={() => isCorrect("option2", option2, answer)}>{questions[current][3]}</div>
+            <div className='options' id="option3" onClick={() => isCorrect("option3", option3, answer)}>{questions[current][4]}</div>
+            <div className='options' id="option4" onClick={() => isCorrect("option4", option4, answer)}>{questions[current][5]}</div>
           </div>
-  })
+        </div>
   
   return (
     <div className="App">
@@ -51,6 +52,7 @@ function App() {
      <h4>Try out this quiz and see how you do!</h4>
      <ul>
       {data}
+      <button onClick={() => setCurrent(current + 1)}> Next </button>
      </ul>
     </div>
   );
