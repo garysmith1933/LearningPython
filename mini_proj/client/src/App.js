e4c74cffe92bc73d45a1a15e15545a2701168580
unknown <garysmith193358@gmail.com>
2023-03-02 18:47:56 -0500

    added function to return string depending on user score

--- mini_proj/client/src/App.js
+++ mini_proj/client/src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+
+const judgeScore = (score) => {
+  const scores = {
+    0: '0/5...uhhh how did you get 0, there is literally 5 questions with 4 options each and somehow you guessed wrong EVERY SINGLE TIME!? HOWWWWW!?!?',
+    1: '1/5 Hey you got 1...NOW GO GET MORE!', 
+    2: '2/5 Meh, you can do better.',
+    3: '3/5 You got more right than you did wrong! Not bad, but not great either!',
+    4: '4/5 Off by one, but 4 is pretty dang good',
+    5: '5/5 You got them all right!! You"re not stalking me are you?'
+  }
+
+  return scores[score]
+}
+
 function App() {
   const [questions, setQuestions] = useState([])
   const [current, setCurrent] = useState(0)
@@ -67,7 +81,11 @@ function App() {
           </div>
           <button onClick={() => nextQuestion()}> Next </button>
         </div>
-  : <p> Your final score is {score}/{questions.length}! Thanks for playing! </p>
+  : <div>
+      <p> {judgeScore(score)}</p>
+      <p>Thanks for playing!</p>
+    </div>
+    
   
   return (
     <div className="App">
