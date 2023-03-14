b5187490b700287d4516e6cd6921eadb1c4a128b
Gary Smith <garysmith193358@gmail.com>
2023-03-13 20:02:32 -0400

    added title, adjusted correct css

--- mini_proj/client/src/App.js
+++ mini_proj/client/src/App.js
@@ -106,8 +106,10 @@ function App() {
     <div id='question-container'>
       <div className='header'>
         <h1>Think you know me?</h1>
-        <h4>Try out this quiz and see how you do!</h4>
+        <p>Try out this quiz and see how you do!</p>
       </div>
+
+      <h3 className='title'> Current question: {questionDetails[0]} </h3>
 
         <div className='questions'>
           <div className='options' id="option1" onClick={() => isCorrect("option1", questionDetails[2], questionDetails[1])}>{questionDetails[2]}</div>
